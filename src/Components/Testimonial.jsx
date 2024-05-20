6814ac1f649a26ff6bf94fba5c03443622cb56e0
Majid Khan <majidkhanuet@gmail.com>
2024-05-20 06:58:33 +0500

    task

---
+++ src/Components/Testimonial.jsx
@@ -0,0 +1,12 @@
+const Testimonial=()=>{
+return (
+    <div className="relative    border-2 border-[#424242]  bg-[#2E2D2E] px-28 py-10   text-white  max-w-[1229px]  mx-auto  h-[584px] rounded-[60px]">
+        <div className="flex flex-col gap-4 border border-green-400 rounded-tl-[200%]">
+            <p className="text-4xl leading-7 font-bold">BayraMeta</p>
+            <p className="text-base font-bold leading-5  text-[#7B7B7B]">BayraMeta World is P2E Metaverse, with the underground FennecVerse only for VIP NFT Owners like fennecs and Upper World for HunterX collections( Blops) & Proton/ ETH approved collections.. We bring exciting games in which you can earn XPR, Bayra whilst existing in the ecosystem. Racing, Shooting, Fighting and other multiplayer games for our VIP users. Concerts and Brand Advertisements for our corporate partners.</p>
+        </div>
+        <div>bottm</div>
+    </div>
+)
+}
+export default Testimonial
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bayra-home': "url('./Images/Bayra_Home_Page.png')",
+      }
+    },
+  },
+  plugins: [],
+}
